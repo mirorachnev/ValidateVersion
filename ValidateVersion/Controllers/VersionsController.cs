1f5120f7da885e03531e632713d63bda2e4a8fe8
Miroslav Rachnev <mrachnev@yahoo.com>
2022-04-19 10:49:59 +0300

    Add project files.

---
+++ ValidateVersion/Controllers/VersionsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ValidateVersion.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VersionsController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public VersionsController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_configuration.GetValue<string>("version"));
+        }
+
+        [HttpGet("{version}")]
+        public IActionResult Get(string version)
+        {
+            return Ok(_configuration.GetValue<string>("version") == version);
+        }
+    }
+}
