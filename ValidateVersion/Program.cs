d654c153f1ca23af9df6fd6f78cc01c061cbcd72
Miroslav Rachnev <mrachnev@yahoo.com>
2022-04-19 11:31:32 +0300

    add cors

--- ValidateVersion/Program.cs
+++ ValidateVersion/Program.cs
@@ -2,9 +2,19 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+var devCorsPolicy = "devCorsPolicy";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(devCorsPolicy, builder => {
+        builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+    });
+});
+
 builder.Services.AddControllers();
 
 var app = builder.Build();
+
+app.UseCors(devCorsPolicy);
 
 // Configure the HTTP request pipeline.
 
